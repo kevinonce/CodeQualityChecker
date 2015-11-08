6c3751522578884668ad51d9ac5db61a2e31bc61
Antoine PHILIPPE <antoine.philippe62@gmail.com>
2015-11-09 00:27:24 +0100

    début du BadInheritanceProcessor, identification des variables ainsi que du type et de celui de l'instance

--- src/processors/BadInheritanceProcessor.java
+++ src/processors/BadInheritanceProcessor.java
@@ -1,5 +1,39 @@
 package processors;
 
-public class BadInheritanceProcessor {
+import java.util.List;
+import java.util.Set;
 
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtElement;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtVariable;
+import spoon.reflect.reference.CtReference;
+import spoon.reflect.visitor.CtInheritanceScanner;
+import spoon.reflect.visitor.Filter;
+import spoon.reflect.visitor.Query;
+import spoon.reflect.visitor.ReferenceFilter;
+import spoon.reflect.visitor.filter.TypeFilter;
+
+public class BadInheritanceProcessor extends AbstractProcessor<CtClass<?>>{
+	private Set<CtMethod<?>> methods;
+	private List<CtVariable<?>> variables;
+	
+	@Override
+	public void process(CtClass<?> ctClass) {
+		methods = ctClass.getAllMethods();
+		//elementList = ctClass.getElements(new FilterImpl());
+		variables = Query.getElements(ctClass, new TypeFilter<CtVariable<?>>(CtVariable.class));
+	}
+	
+	@Override
+	 public void processingDone() {
+		CtInheritanceScanner sc = new CtInheritanceScanner() {
+		};
+			for(CtVariable<?> c : variables){
+				System.out.println(c.getDefaultExpression().getType());
+				System.out.println(c.getType());
+
+			}
+	 }
 }
