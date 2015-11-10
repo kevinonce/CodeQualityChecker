fa30e606c337497ddefe8ac1915fa3adacdd6495
Mandrivia <antoine.philippe62@gmail.com>
2015-11-10 16:26:20 +0100

    ajout parseur de méthodes appelées et disponile dans chaque superclasse/instance

--- src/processors/BadInheritanceProcessor.java
+++ src/processors/BadInheritanceProcessor.java
@@ -1,6 +1,7 @@
 package processors;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.Map;
 import java.util.Set;
 
 import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.CtInvocation;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtVariable;
 import spoon.reflect.reference.CtExecutableReference;
@@ -19,7 +21,7 @@ public class BadInheritanceProcessor extends AbstractProcessor<CtClass<?>>{
 
 	private List<CtVariable<?>> variables;
 	private Set<CtTypeReference<?>> itf; 
-	private Set<CtTypeReference<?>> cl;
+	private List<CtInvocation<?>> invocations;
 	private Map<Method, CtTypeReference<?>> methods;
 	
 	@Override
@@ -27,17 +29,16 @@ public class BadInheritanceProcessor extends AbstractProcessor<CtClass<?>>{
 		//elementList = ctClass.getElements(new FilterImpl());
 		variables = Query.getElements(ctClass, new TypeFilter<CtVariable<?>>(CtVariable.class));
 		methods = new HashMap<Method, CtTypeReference<?>>();
+		invocations = Query.getElements(ctClass, new TypeFilter<CtInvocation<?>>(CtInvocation.class));
 
 	}
 	
 	@Override
 	 public void processingDone() {
-		Set<CtTypeReference<?>> itf = new HashSet<CtTypeReference<?>>();
-		Set<CtTypeReference<?>> cl = new HashSet<CtTypeReference<?>>();
+
 		for(CtVariable<?> c : variables){
 			//System.out.println("v : " + c.getSignature());
 			itf = new HashSet<CtTypeReference<?>>();
-			cl = new HashSet<CtTypeReference<?>>();
 			
 			itf = c.getType().getSuperInterfaces();
 			itf.add(c.getType().getSuperclass());
@@ -45,23 +46,32 @@ public class BadInheritanceProcessor extends AbstractProcessor<CtClass<?>>{
 				//System.out.println("itf : " + type.getSimpleName());
 				//System.out.println("itf : " + type);
 				for(CtExecutableReference<?> e : type.getDeclaredExecutables()){
-					methods.put(e.getActualMethod(), type);
-					System.out.println(e.getActualMethod() + ":" +  type);
+					if(e.getActualMethod() != null){
+						methods.put(e.getActualMethod(), type);
+						//System.out.println(e.getActualMethod() + ":" +  type);
+					}
 				}
-				//System.out.println(type.getDeclaredExecutables());
-				//for(CtMethod<?> m : type.getSimpleName())
-				//methods.put(key, type);
 			}
 			
-			
+			//System.out.println(c);
 		}
-	
-		/*for(CtVariable<?> c : variables){
-			System.out.println(c.getType());
-			if(c.getDefaultExpression() != null){
-				System.out.println(c.getType().getSuperInterfaces());
-				System.out.println(c.getType().getSuperclass());
+		
+		for(CtInvocation<?> c : invocations){
+			for(Method m : methods.keySet()){
+				if(m != null && c.getExecutable().getActualMethod() != null){
+//				if(m.toString().split(".")[3].equals(c.getExecutable().getActualMethod().toString().split(".")[3]))
+					System.out.print(m.getName());
+						for(Parameter p : m.getParameters()){
+							System.out.print(p.getType().getSimpleName() + ",");
+						}
+					System.out.println(")"  + " <=> " + c.getExecutable().getActualMethod().getName());
+				}
 			}
-		}*/
+			//System.out.println(c.getExecutable().getActualMethod());
+			//System.out.println(c.getExecutable().getSimpleName());
+			//System.out.println(c.getTarget());
+		}
+		
+		
 	 }
 }
