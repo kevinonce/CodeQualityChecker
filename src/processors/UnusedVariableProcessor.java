b2e7a70ba0f7b07239ac79e05b47f9e05e4b84f1
Antoine PHILIPPE <antoine.philippe62@gmail.com>
2015-11-16 01:44:49 +0100

    Ajout UnusedVariableProcessor : détecte encore trop de faux positifs (variables avec accès statique ou cas avec getters/setters)

--- src/processors/UnusedVariableProcessor.java
+++ src/processors/UnusedVariableProcessor.java
@@ -1,5 +1,35 @@
 package processors;
 
-public class UnusedVariableProcessor {
+import java.util.LinkedList;
+import java.util.List;
 
+import filter.InvocationsByVariableFilter;
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.CtInvocation;
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtVariable;
+import spoon.reflect.visitor.Query;
+import spoon.reflect.visitor.filter.TypeFilter;
+
+public class UnusedVariableProcessor extends AbstractProcessor<CtClass<?>> {
+
+	private List<CtVariable<?>> variables = new LinkedList<CtVariable<?>>();
+	private List<CtInvocation<?>> invocations = new LinkedList<CtInvocation<?>>();
+
+	@Override
+	public void process(CtClass<?> ctClass) {
+		variables.addAll(Query.getElements(ctClass, new TypeFilter<CtVariable<?>>(CtVariable.class)));
+
+	}
+
+	public void processingDone() {
+		for (CtVariable<?> v : variables) {
+			if (v.getType() != null && v.getVisibility() != null) {
+				invocations = v.getParent(CtClass.class).getElements(new InvocationsByVariableFilter(v));
+				if (invocations.isEmpty())
+					System.out.println("----------TROUVÉ--------\nVariable : " + v + "\nSource : "
+							+ v.getPosition());
+			}
+		}
+	}
 }
